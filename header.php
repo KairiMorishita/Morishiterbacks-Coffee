0cb56087658ee75a142a65959323b153b84daf84
Kairi Morishita <k.moricchifc73@gmail.com>
2020-03-23 22:47:25 +0900

    Chande characterset

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
-		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" charset="UTF-8">
 		<title>Morishiterbacks Coffee</title>
 		<!--リセット-->
 		<link rel="stylesheet" href="css/reset.css">
