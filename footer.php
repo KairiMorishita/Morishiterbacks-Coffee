da042d93464fc801e2f1694a788835b83234a60b
Kairi Morishita <k.moricchifc73@gmail.com>
2020-04-01 21:50:50 +0900

    Remove something

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer class="global">
 		<ul class="clearfix">
 			<li><a href="allergy.php">ALLERGY&nbsp;and&nbsp;NUTRITION&nbsp;INFO<span><br>アレルギー・栄養成分</span></a></li>
-			<li><a href="contact.html">CONTACT<span><br>お問い合わせ</span></a></li>
 			<li><a href="policy.php">POLICY<span><br>個人情報保護方針</a></li>
 			<li><a href="torihikihou.php">LEGAL INFO<span><br>特定商取引法に基づく表記</a></li>
 			<li><a href="recruit.php">RECRUIT<span><br>スタッフ募集</span></a></li>
